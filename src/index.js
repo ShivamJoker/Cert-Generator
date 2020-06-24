a49702fa5a5399d23e68a0b5673d148ab29662b0
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-06-24 14:56:37 +0530

    completed the cert app

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import { PDFDocument, rgb, degrees } from "pdf-lib";
+import FileSaver from "file-saver";
+import fontkit from "@pdf-lib/fontkit";
+
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener("click", () => {
+  const val = capitalize(userName.value);
+
+  //check if the text is empty or not
+  if (val.trim() !== "" && userName.checkValidity()) {
+    // console.log(val);
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch(require("url:./cert.pdf")).then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  //get font
+  const fontBytes = await fetch(
+    require("url:./Sanchez-Regular.ttf")
+  ).then((res) => res.arrayBuffer());
+
+  // Embed our custom font in the document
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Draw a string of text diagonally across the first page
+  firstPage.drawText(name, {
+    x: 300,
+    y: 270,
+    size: 58,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+  console.log("Done creating");
+
+  // this was for creating uri and showing in iframe
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("pdf").src = pdfDataUri;
+
+  var file = new File([pdfBytes], "Padhega India Subscription Certificate.pdf", {
+    type: "application/pdf;charset=utf-8",
+  });
+  FileSaver.saveAs(file);
+};
+
+// init();
